d055bb0791849148968bf9045d5294db1f153b94
BIPLOB <developerbiplob.bd@gmail.com>
2021-09-09 13:45:46 +0600

    Model Event | Define Event Class | Closer

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Events\DataCreateEvent;
 use App\Scopes\ViewScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -66,6 +67,33 @@ class Product extends Model
         return $query->where('status', '=', $status);
     }
 
+
+    // Model Event Wiht Define Dadicated Class-----------//
+    // kono akta kaj complite howar por kicho korte cahile ai Event er maddome korbo.
+    // Data ta create howar por ki korte chaschi seta Event class er modde bole dibo.
+    // like- Data save howar por user ke mail send kora, kono notification show kora.
+    // Eikhane $dispatchsEvent e sob Event guloke defien kore dibo. sathe method define kore dibo. 
+    // Like - create hole kon Event ta kaj korbe, ta update hole konta, or delete hole konta. ai vabe.
+
+    protected $dispatchesEvents = [
+        'created' => DataCreateEvent::class,  
+
+    ];
+
+
+
+    // Model Event Define With Closer-----------//
+    // Ai vabe use korle Extra class make korte hobe na, just booted function ki korte chai bole dibo.
+    protected static function booted()
+    {
+        static::created(function ($product) {
+            info('Product Data created Successfllly by Closer');
+        });
+
+        static::retrieved(function ($product) { // get korle ai retrieved ta call hobe.
+            info('Product Data Show');
+        });
+    }
 
 
 }
