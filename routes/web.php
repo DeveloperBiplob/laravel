c8cccde8bef81e2ff333b4ac0f98ec83fdaa8bbe
BIPLOB <developerbiplob.bd@gmail.com>
2021-10-05 20:09:57 +0600

    Route::post('/email/verification-notification', function (Request ) {

--- routes/web.php
+++ routes/web.php
@@ -591,3 +591,13 @@ Route::get('/scope', function () {
     
         return redirect('/dashboard');
     })->middleware(['auth', 'signed'])->name('verification.verify');
+
+
+
+    // Resending The Verification Email-------------//
+
+    Route::post('/email/verification-notification', function (Request $request) {
+        $request->user()->sendEmailVerificationNotification();
+    
+        return back()->with('message', 'Verification link sent!');
+    })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
